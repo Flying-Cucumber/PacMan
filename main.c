7cdb24ddda8448a5d577935151c7e4f98365bd1d
Thomas <warbeaver@pm.me>
2019-04-02 16:30:47 +0200

    ?

--- main.c
+++ main.c
@@ -2,13 +2,8 @@
 #include "terrain.h"
 #include "entities.h"
 
-<<<<<<< HEAD
-void pacman_move_up(pcm){
-
-=======
 void pacman_move_up(struct pacman* pcm){
     pcm->current_slab = pcm->current_slab->up;
->>>>>>> 170b4042ef084581f9cbf43a0529800a1ebdba32
 }
 
 int main(){
