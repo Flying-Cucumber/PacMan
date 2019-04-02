9fe02a73138465047b09f9aa9a307dadcec7d1fb
Thomas <warbeaver@pm.me>
2019-04-02 16:08:27 +0200

    entities

---
+++ entities.h
@@ -0,0 +1,19 @@
+#ifndef __ENTITIES_H__
+#define __ENTITIES_H__
+#include <terrain.h>
+#include <stdbool.h>
+
+struct pacman {
+    int x
+    int y
+    slab slapo
+    bool is_alive
+    bool is_supersaiyen
+}
+
+struct ghost {
+    int x
+    int y
+    slab slapo
+    bool is_alive
+}
