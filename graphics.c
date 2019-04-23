b448507d5472d703a8b10de8aadb78257ff3035d
Quentin KREMPP <quentin.krempp@enta-paristech.fr>
2019-04-23 16:10:34 +0200

    not closing window

--- graphics.c
+++ graphics.c
@@ -1,8 +1,11 @@
 #include <SDL/SDL.h>
 #include "game.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-void start_interface(struct game* g){
+void pause();
+
+int start_interface(struct game* g){
     
     if (SDL_Init(SDL_INIT_VIDEO) == -1){
         fprintf(stderr, "Erreur d'initialisation de la SDL: %s\n", SDL_GetError);
@@ -11,5 +14,22 @@ void start_interface(struct game* g){
 
     SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
 
+    pause();
+
     SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
+void pause(){
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer){
+        SDL_WaitEvent(&event);
+        switch(event.type){
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
 }
