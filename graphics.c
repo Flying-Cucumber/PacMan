fdc06f61f3a3286937ac2366b4b59b154ed181a0
Thomas <warbeaver@pm.me>
2019-05-05 19:11:30 +0200

    a

--- graphics.c
+++ graphics.c
@@ -1,9 +1,17 @@
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #include "game.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 void pause();
+
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination){ 
+    SDL_Rect offset;
+	offset.x = x; 
+	offset.y = y;
+	SDL_BlitSurface(source, NULL, destination, &offset); 
+}
 
 int start_interface(struct game* g){
 
@@ -12,19 +20,30 @@ int start_interface(struct game* g){
         exit(EXIT_FAILURE);
     }
 
-    SDL_Surface *ecran = NULL;
+    SDL_Surface *screen = NULL;
+    SDL_Surface *field = NULL;
+    SDL_Surface *pacman_surf = NULL;
+    SDL_Surface *blinky_surf = NULL;
+    SDL_Surface *pinky_surf = NULL;
+    SDL_Surface *inky_surf = NULL;
+    SDL_Surface *clyde_surf = NULL; 
 
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
-    if (ecran == NULL){
+    field = IMG_Load("Media/Pictures/field.jpg");
+
+
+    screen = SDL_SetVideoMode(520, 600, 32, SDL_HWSURFACE);
+    if (screen == NULL){
         fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
-    Uint32 handsome_color = SDL_MapRGB(ecran->format, 0, 255, 0);
-    SDL_FillRect(ecran, NULL, handsome_color);
-    SDL_Flip(ecran);
+    Uint32 handsome_color = SDL_MapRGB(screen->format, 13, 26, 52);
+    SDL_FillRect(screen, NULL, handsome_color);
+    SDL_Flip(screen);
 
-    SDL_WM_SetCaption("Intelligence artificielle pour Hanabi", NULL);
+    apply_surface(0, 0, field, screen);
+
+    SDL_WM_SetCaption("Pac-Man", NULL);
 
     pause();
 
