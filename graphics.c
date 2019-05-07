2377e83f3b12bfa1d8d55768534ba64892f7ba97
Quentin KREMPP <quentin.krempp@enta-paristech.fr>
2019-05-07 16:20:05 +0200

    graphics restructuration

--- graphics.c
+++ graphics.c
@@ -6,13 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-SDL_Surface* draw_rectangle(SDL_Surface* background, int coord_x, int coord_y, int size_x, int size_y, int color_red, int color_green, int color_blue);
-void paint_terrain(SDL_Surface* background, struct terrain* t);
-void draw_slab(SDL_Surface* background, struct slab* current_slab);
-void paint_entities(SDL_Surface* background, struct game* g);
-
-int start_interface(SDL_Surface* background, struct game* g){
+int start_interface(SDL_Surface* background, Game* g){
 
     paint_terrain(background, g->t);
     paint_entities(background, g);
@@ -36,9 +30,9 @@ SDL_Surface* draw_rectangle(SDL_Surface* background, int coord_x, int coord_y, i
     return rectangle;
 }
 
-void paint_terrain(SDL_Surface* background, struct terrain* t){ //Parcoure toute les slab pour les afficher à l'écran
-    struct slab* current_slab = t->initial_slab;
-    struct slab* top_slab;
+void paint_terrain(SDL_Surface* background, Terrain* t){ //Parcoure toute les slab pour les afficher à l'écran
+    Slab* current_slab = t->initial_slab;
+    Slab* top_slab;
 
     while(current_slab != NULL){
         top_slab = current_slab;
@@ -53,47 +47,48 @@ void paint_terrain(SDL_Surface* background, struct terrain* t){ //Parcoure toute
     }
 }
 
-SDL_Surface*[] draw_slab(SDL_Surface* background, struct slab* current_slab){ //Représente les slab selon leur type
+void draw_slab(SDL_Surface* background, Slab* current_slab){ //Représente les slab selon leur type
     
     SDL_Surface* affichage;
     SDL_Surface* objet;
 
     switch (current_slab->type){
         case WALL:
-            affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 255);
-            objet = NULL;
+            current_slab->affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 255);
+            current_slab->objet = NULL;
             break;
         case PAC_GUM:
-            affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
-            objet = draw_rectangle(background, ((3 * current_slab->x + 1 ) * SLAB_SIZE) / 3, ((3 * current_slab->y + 1 ) * SLAB_SIZE) / 3, SLAB_SIZE / 3, SLAB_SIZE / 3, 255, 255, 255);
+            current_slab->affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
+            current_slab->objet = draw_rectangle(background, ((3 * current_slab->x + 1 ) * SLAB_SIZE) / 3, ((3 * current_slab->y + 1 ) * SLAB_SIZE) / 3, SLAB_SIZE / 3, SLAB_SIZE / 3, 255, 255, 255);
             break;
         case SUPER_PAC_GUM:
-            affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
-            objet = draw_rectangle(background, ((5 * current_slab->x + 1) * SLAB_SIZE) / 5, ((5 * current_slab->y + 1) * SLAB_SIZE) / 5, 3 * SLAB_SIZE / 5, 3 * SLAB_SIZE / 5, 255, 255, 255);
+            current_slab->affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
+            current_slab->objet = draw_rectangle(background, ((5 * current_slab->x + 1) * SLAB_SIZE) / 5, ((5 * current_slab->y + 1) * SLAB_SIZE) / 5, 3 * SLAB_SIZE / 5, 3 * SLAB_SIZE / 5, 255, 255, 255);
             break;
         default:
-            affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
-            objet = NULL;
+            current_slab->affichage = draw_rectangle(background, current_slab->x * SLAB_SIZE, current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 0, 0);
+            current_slab->objet = NULL;
             break;
     }
     printf("Dalle (%d, %d) représentée\n", current_slab->x, current_slab->y);
-    return [affichage, objet];
 }
 
-void paint_pacman(SDL_Surface* background, struct pacman* p){
-    draw_rectangle(background, p->self->current_slab->x * SLAB_SIZE, p->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 255, 0);
+void paint_pacman(SDL_Surface* background, Pacman* p){
+
+    p->affichage = draw_rectangle(background, p->self->current_slab->x * SLAB_SIZE, p->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 255, 0);
 }
 
-void paint_ghost(SDL_Surface* background, struct game* g){
+void paint_ghost(SDL_Surface* background, Game* g){
 
-    draw_rectangle(background, g->blinky->self->current_slab->x * SLAB_SIZE, g->blinky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 0, 0);
-    draw_rectangle(background, g->inky->self->current_slab->x * SLAB_SIZE, g->inky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 255, 0);
-    draw_rectangle(background, g->pinky->self->current_slab->x * SLAB_SIZE, g->pinky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 0, 255);
-    draw_rectangle(background, g->clyde->self->current_slab->x * SLAB_SIZE, g->clyde->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 255, 0);
+    g->blinky->affichage = draw_rectangle(background, g->blinky->self->current_slab->x * SLAB_SIZE, g->blinky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 0, 0);
+    g->inky->affichage = draw_rectangle(background, g->inky->self->current_slab->x * SLAB_SIZE, g->inky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 0, 255, 0);
+    g->pinky->affichage = draw_rectangle(background, g->pinky->self->current_slab->x * SLAB_SIZE, g->pinky->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 0, 255);
+    g->clyde->affichage = draw_rectangle(background, g->clyde->self->current_slab->x * SLAB_SIZE, g->clyde->self->current_slab->y * SLAB_SIZE, SLAB_SIZE, SLAB_SIZE, 255, 255, 0);
 
 }
 
-void paint_entities(SDL_Surface* background, struct game* g){
+void paint_entities(SDL_Surface* background, Game* g){
+
     paint_pacman(background, g->p);
     paint_ghost(background, g);
 }
